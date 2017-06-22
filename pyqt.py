07d90b36194be55c610aaa67cfd0d81eb3e25bb6
Arthur Moore <arthur.moore85@gmail.com>
2017-06-22 09:25:26 +0100

    Update pyqt.py

--- pyqt.py
+++ pyqt.py
@@ -1,14 +1,24 @@
+"""
+.. module: pyqt
+.. description: Main controller file for managing a PyQt app.
+.. author: Arthur Moore <arthur.moore85@gmail.com>
+.. License: GPLv2
+"""
+
 from PyQt4 import QtGui, QtCore, QtWebKit
 from PyQt4.QtCore import *
-from mainWindow import Ui_MainWindow # or what it says in module
+from mainWindow import Ui_MainWindow # Class of mainWindow UI python module.
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         """
-        Some comment here
+        MainWindow class.
+        This controls the UI for PyQt
         """
         QtGui.QWidget.__init__(self, parent)
         self.setupUi(self)
+        
+        # The following can be used for multiple windows.
         self.window1 = None
         self.window2 = None
         self.window3 = None
